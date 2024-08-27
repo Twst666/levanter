defc8730fcc7b837a2541f4c90aae57cad1b1c1a
Xoul ser <131429508+Xoulfff@users.noreply.github.com>
2024-08-27 19:06:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -32,13 +32,7 @@ module.exports = {
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
-  STICKER_PACKNAME: process.env.STICKER_PACKNAME || '⺀𑁍 𝑺 𝚨 𝜝 𝜜 𝜞 𝜤  ▸ᝃ〔🧃〕;
-
-〝 ┊𝚻ʜᴇ 𝐌ᴏᴏɴ 𝐈s 𝚩ᴇᴜᴛɪғᴜʟ ...🌝
-〝 ┊
-〝 ┊𝐈ꜱɴ'ᴛ  𝐈ᴛ ?💐
-〝 ┊
-〝 ┊⁹⁶³³⁶⁰⁵⁶⁴⁸ 🍭',
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || '⺀𑁍 𝑺 𝚨 𝜝 𝜜 𝜞 𝜤  ▸ᝃ〔🧃〕',
   ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
   LOG_MSG: toBool(process.env.LOG_MSG) || false,
   RMBG_KEY: process.env.RMBG_KEY || 'null',
